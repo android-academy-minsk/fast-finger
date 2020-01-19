c624d3771b6cd030d974b5bf40a735eb16fda739
vadzimv <vysotski.vadim@gmail.com>
2020-01-19 18:17:50 +0300

    initial commit

---
+++ finished_code/src/main/java/by/android/academy/minsk/fastfinger/MainActivity.kt
@@ -0,0 +1,19 @@
+package by.android.academy.minsk.fastfinger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.android.academy.minsk.fastfinger.ui.main.GameFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, GameFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ finished_code/src/main/java/by/android/academy/minsk/fastfinger/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package by.android.academy.minsk.fastfinger.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
