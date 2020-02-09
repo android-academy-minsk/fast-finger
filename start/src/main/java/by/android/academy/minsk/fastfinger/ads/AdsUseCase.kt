cee671848b324e32baf0e7e26bd28c5cf528ee93
vadzimv <vysotski.vadim@gmail.com>
2020-02-09 12:54:54 +0300

    moved todo inside

--- start/src/main/java/by/android/academy/minsk/fastfinger/ads/AdsUseCase.kt
+++ start/src/main/java/by/android/academy/minsk/fastfinger/ads/AdsUseCase.kt
@@ -1,8 +1,8 @@
 package by.android.academy.minsk.fastfinger.ads
 
 class AdsUseCase(private val api: AdsApi) {
-    //TODO(12): ads business logic. Return ShowAdsResult using api.
     suspend fun showAds(): ShowAdsResult {
+        //TODO(12): ads business logic. Return ShowAdsResult using api.
         TODO()
     }
 }
